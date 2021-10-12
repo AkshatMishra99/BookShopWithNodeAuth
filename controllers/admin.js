94e14ca07c5956419a33aedc5eab5853acfbac9b
Akshat Mishra <mailakshat99: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@gmail.com>
2021-10-12 22:44:49 +0530

    added validation in add and edit product forms

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,15 +1,33 @@
 const Product = require("../models/product");
-
+const { validationResult } = require("express-validator");
 exports.getAddProduct = (req, res, next) => {
 	res.render("admin/edit-product", {
 		pageTitle: "Add Product",
 		path: "/admin/add-product",
 		editing: false,
-		isAuthenticated: req.session.isLoggedIn
+		isAuthenticated: req.session.isLoggedIn,
+		errorMessage: undefined,
+		validationErrors: [],
+		userInput: undefined,
+		hasError: false
 	});
 };
 
 exports.postAddProduct = (req, res, next) => {
+	const errors = validationResult(req).array();
+	if (errors.length > 0) {
+		const message = errors[0].msg;
+		return res.status(422).render("admin/edit-product", {
+			pageTitle: "Add Product",
+			path: "/admin/add-product",
+			editing: false,
+			isAuthenticated: req.session.isLoggedIn,
+			errorMessage: message,
+			validationErrors: errors,
+			hasError: true,
+			product: { ...req.body }
+		});
+	}
 	const title = req.body.title;
 	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
@@ -49,13 +67,30 @@ exports.getEditProduct = (req, res, next) => {
 				path: "/admin/edit-product",
 				editing: editMode,
 				product: product,
-				isAuthenticated: req.session.isLoggedIn
+				errorMessage: undefined,
+				validationErrors: [],
+				userInput: undefined,
+				hasError: false
 			});
 		})
 		.catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
+	const errors = validationResult(req).array();
+	if (errors.length > 0) {
+		const message = errors[0].msg;
+		return res.status(422).render("admin/edit-product", {
+			pageTitle: "Edit Product",
+			path: "/admin/edit-product",
+			editing: true,
+			isAuthenticated: req.session.isLoggedIn,
+			errorMessage: message,
+			validationErrors: errors,
+			product: { ...req.body },
+			hasError: true
+		});
+	}
 	const prodId = req.body.productId;
 	const updatedTitle = req.body.title;
 	const updatedPrice = req.body.price;
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const express = require("express");
-
+const { body } = require("express-validator");
 const adminController = require("../controllers/admin");
 const isAuth = require("../middleware/is-auth");
 
@@ -14,11 +14,37 @@ router.get("/add-product", isAuth, adminController.getAddProduct);
 router.get("/products", isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post(
+	"/add-product",
+	isAuth,
+	body("title", "Enter valid title!!").trim().isLength({ min: 1, max: 400 }),
+	body("imageUrl", "Enter valid image url!!")
+		.isURL()
+		.trim()
+		.isLength({ min: 1 }),
+	body("price", "Enter valid price!!").isNumeric(),
+	body("description", "Enter valid description!!")
+		.trim()
+		.isLength({ min: 5, max: 400 }),
+	adminController.postAddProduct
+);
 
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post(
+	"/edit-product",
+	isAuth,
+	body("title", "Enter valid title!!").trim().isLength({ min: 1, max: 400 }),
+	body("imageUrl", "Enter valid image url!!")
+		.isURL()
+		.trim()
+		.isLength({ min: 1 }),
+	body("price", "Enter valid price!!").isNumeric(),
+	body("description", "Enter valid description!!")
+		.trim()
+		.isLength({ min: 5, max: 400 }),
+	adminController.postEditProduct
+);
 
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
